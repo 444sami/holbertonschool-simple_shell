1ebb5bc066c5329d77a15a41643bcff20fdb4cd8
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-04-24 11:54:53 -0700

    spliting headers

--- shell.h
+++ shell.h
@@ -1,27 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* libraries */
-#include <sys/types.h>
-#include <sys/stat.h>
-
-#include <unistd.h>
-
-/* structures */
-
-/**
- * struct word_list- a list of words
- * @word: the word
- * @len: words length
- * @next: next word
- */
-typedef struct word_list
-{
-	char *word;
-	int len;
-	struct word_list *next;
-} wl_t;
-
+#include "libraries.h"
 
 /* Prototypes */
 
@@ -31,11 +11,5 @@ void interactive_mode(int mode, int ac, char **av, char **env);
 
 /* aux_func.c */
 char **cmd_to_arg(void);
-
-/* wl_man.c */
-wl_t *create_list(char *str);
-void *free_list(wl_t *l, int flag);
-int words_counter(wl_t *l);
-void print_list(wl_t *l);
 
 #endif
