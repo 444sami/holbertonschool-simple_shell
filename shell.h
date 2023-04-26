fe59f6fa649834b077b47350ab1568aa8abaabe4
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-04-25 21:21:33 -0700

    joining headers

--- shell.h
+++ shell.h
@@ -1,9 +1,34 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef SHELL_H
+#define SHELL_H
 
-#include "libraries.h"
+/*---->    Included Libraries    <----*/
+#include <sys/types.h>
+#include <sys/stat.h>
 
-/* Prototypes */
+#include <unistd.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/*---->    Structures    <----*/
+
+/**
+ * struct word_list- a list of words
+ * @word: the word
+ * @len: words length
+ * @next: next word
+ */
+typedef struct word_list
+{
+	char *word;
+	int len;
+	struct word_list *next;
+} wl_t;
+
+
+/*---->     Prototypes     <----*/
 
 /* _simple_shell.c */
 int main(int ac, char **av, char **envron);
@@ -16,5 +41,11 @@ char **free_cmds(char **cmds);
 void err_han(char *s1, char *s2);
 
 /* built_in.c */
+
+/* low.c */
+wl_t *create_list(char *str);
+void *free_list(wl_t *l, int flag);
+int words_counter(wl_t *l);
+void print_list(wl_t *l);
 
 #endif
