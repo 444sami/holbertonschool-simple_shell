83632df167c13369be70af03a44159d27d2b3a78
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-04-25 21:05:04 -0700

    update in prototypes and files

--- shell.h
+++ shell.h
@@ -6,9 +6,15 @@
 /* Prototypes */
 
 /* _simple_shell.c */
-int main(void);
+int main(int ac, char **av, char **envron);
 
 /* aux_func.c */
 char **cmd_to_arg(void);
+char **free_cmds(char **cmds);
+
+/* err.c */
+void err_han(char *s1, char *s2);
+
+/* built_in.c */
 
 #endif
