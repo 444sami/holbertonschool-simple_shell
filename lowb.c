48ef3611453fbf752e786644aa0426c8f0232a65
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-04-27 05:02:50 -0700

    spliting low.c

---
+++ lowb.c
@@ -0,0 +1,46 @@
+#include "shell.h"
+
+/**
+ * free_list- frees a words list
+ * @l: words list
+ * @flag: if 0 does't free words, frees otherwise
+ * Return: null
+ */
+void *free_list(wl_t *l, int flag)
+{
+	if (l)
+	{
+		free_list(l->next, flag);
+		if (flag)
+			free(l->word);
+		free(l);
+	}
+	return (NULL);
+}
+
+/**
+ * words_counter- counts the amount of words
+ * @l: list of words
+ * Return: the amount of words
+ */
+int words_counter(wl_t *l)
+{
+	if (!l)
+		return (0);
+	else
+		return (1 + words_counter(l->next));
+}
+
+/**
+ * print_list- prints a list of words
+ * @l: list of words
+ * Return: void
+ */
+void print_list(wl_t *l)
+{
+	if (l)
+	{
+		printf("%s\n", l->word);
+		print_list(l->next);
+	}
+}
