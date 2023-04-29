c32ef01b6a658fe3670ee5f1fdffd260caa5daa0
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-04-28 23:13:35 -0700

    spltting functions in two different files

--- commands_main.c
+++ commands_main.c
@@ -44,39 +44,3 @@ char **args_arr(l_node *l)
 	free_list(l, 0);
 	return (args);
 }
-
-/**
- */
-
-
-/**
- * free_args- frees args
- * @args: args to be freed
- * Return: void
- */
-void free_args(char **args)
-{
-	int	i;
-
-	if (args)
-	{
-		for (i = 0; args[i]; i++)
-			free(args[i]);
-		free(args[i]);
-		free(args);
-	}
-}
-
-/**
- * print_args- prints array of args
- * @args: array of args
- * Return: void
- */
-void print_args(char **args)
-{
-	int	i;
-
-	if (args)
-		for (i = 0; args[i]; i++)
-			printf("%s\n", args[i]);
-}
